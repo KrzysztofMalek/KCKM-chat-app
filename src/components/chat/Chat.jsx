2c71b25752a1d09810d8615cd1c1a74fa0135f26
Your Name <you@example.com>
2024-10-31 13:34:40 +0100

    Wstępma Strona Chat.jsx

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,7 +1,24 @@
 import "./chat.css"
 const Chat = () => {
     return (
-        <div className='chat'>Chat</div>
+        <div className='chat'>
+            <div className="top">
+                <div className="user">
+                    <img src="./avatar.png" alt="" />
+                    <div className="texts">
+                        <span>Krzysztof Małek</span>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
+                    </div>
+                </div>
+                <div className="icons">
+                    <img src="./phone.png" alt="" />
+                    <img src="./video.png" alt="" />
+                    <img src="./info.png" alt="" />
+                </div>
+            </div>
+            <div className="center"></div>
+            <div className="bootom"></div>
+        </div>
     )
 }
 
